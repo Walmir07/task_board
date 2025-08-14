af1984dfb4b65bd18abe2ffde573d1c192f8d3a4
Walmir07 <walmir.lima@academico.ifpb.edu.br>
2025-08-14 20:15:33 -0300

    Realizando tratamento de erros

---
+++ app/src/main/java/br/com/board/exception/CardFinishedException.java
@@ -0,0 +1,9 @@
+package br.com.board.exception;
+
+public class CardFinishedException extends RuntimeException{
+
+    public CardFinishedException(final String message) {
+        super(message);
+    }
+    
+}
